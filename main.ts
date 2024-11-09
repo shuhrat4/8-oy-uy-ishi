ed63b1fc7851eaabc745cbd2facfa955fca238b4
shuhrat4 <xudoyberdiyevshuhrat5@gmail.com>
2024-11-10 00:46:48 +0500

    d

---
+++ main.ts
@@ -0,0 +1,50 @@
+// 1
+
+// let newValue: string | null = prompt("Qiymat kiriting");
+// let list: string[] = [];
+
+// function collectList(value: string | null): void {
+//     if (value) {
+//         list.push(value);
+//     }
+// }
+
+// collectList(newValue);
+// console.log(list); 
+
+// 2
+
+// let numberList:number[]= [21, 32, 45, 54, 67, 87]
+
+// function findMaxNumber(arr:number[]):number{
+//     let maxNum:number = arr [0]
+//     for(let num of arr ){
+//         if(num>=maxNum ){
+//             maxNum = num
+//         }
+//     }
+//     return maxNum
+// }
+// const result:number = findMaxNumber(numberList)
+// console.log(result);
+
+// 3
+
+// let value:string | null = prompt('soz kiriting')
+// let list:string[] = ['salom', 'yaxshi', 'zor', ]
+
+// function findvalue(newValue:string | null , arr:string[]):void{
+//     let count:number = 0 
+//     arr.forEach((item:string)=>{
+//         if(item == newValue){
+//             count++
+//         }
+//     })
+//     console.log(count > 0 ? true : false);
+// }
+// findvalue(value,list)
+
+// 4
+
+let numberList : number [] = [21,43,56,43]
+
